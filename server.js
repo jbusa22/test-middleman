4b7989c7ac1cfd3acb2e388caa25ce7c39d04568
Jack Busa <jbusa@umich.edu>
2021-05-13 14:37:06 -0400

    testing rest endpoint

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
 console.log("hi")
 // cool
+const express = require('express')
+
+const app = express()
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.listen(3000, (err) => {
+    if (err) throw err
+    console.log('Server running in http://127.0.0.1:3000')
+})
